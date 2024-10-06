cd0d514b56a97b4a1c69a5f4c7c7f4fdac8fc71e
aaronkoichi <aaronlee4399@gmail.com>
2024-10-06 22:14:36 +0800

    added gruvbox theme

--- init.lua
+++ init.lua
@@ -454,7 +454,7 @@ require('lazy').setup({
 
       require('lualine').setup {
         options = {
-          theme = rose_pine,
+          theme = 'gruvbox-baby',
           component_separators = '|',
           section_separators = { left = '', right = '' },
         },
@@ -932,15 +932,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'luisiacc/gruvbox-baby',
+    name = 'gruvbox-baby',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'rose-pine'
-
+      vim.o.background = 'dark' -- or "light" for light mode
+      -- vim.g.gruvbox_material_background = 'hard'
+      vim.cmd.colorscheme 'gruvbox-baby'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
